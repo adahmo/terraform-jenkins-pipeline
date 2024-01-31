d731e90a9981ff8b8832ea982786b5b3bb320702
adahmo <118616463+adahmo@users.noreply.github.com>
2024-01-30 19:06:57 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "aws_access_key" {
   description = "AWS access key"
   type        = string
-  default     = ""
+  default     = "jenkins-aws"
 }
 
 variable "aws_secret_key" {
   description = "AWS secret key"
   type        = string
-  default     = ""
+  default     = "jenkins-aws"
 }
 
 variable "aws_region" {
